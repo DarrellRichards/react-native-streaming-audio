de1ac8d7f78085071174f25131834cb8d7389a30
Darrell Richards <drichards@valcom.com>
2019-03-10 22:47:06 -0400

    Added full support for starting and stopping audio stream

--- android/src/main/java/com/reactlibrary/RNStreamingAudioModule.java
+++ android/src/main/java/com/reactlibrary/RNStreamingAudioModule.java
@@ -1,14 +1,20 @@
 
 package com.reactlibrary;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.io.IOException;
+
 public class RNStreamingAudioModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+  MediaPlayer mediaPlayer = new MediaPlayer();
 
   public RNStreamingAudioModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -21,11 +27,25 @@ public class RNStreamingAudioModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getDeviceName(Callback cb) {
-    try{
-      cb.invoke(null, android.os.Build.MODEL);
-    }catch (Exception e){
-      cb.invoke(e.toString(), null);
-    }
+  public void start(String url, Callback cb) throws IOException {
+      String myUri = url;
+      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+      try {
+          mediaPlayer.setDataSource(myUri);
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      try {
+          mediaPlayer.prepare();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+
+      mediaPlayer.start();
+  }
+
+  @ReactMethod
+  public void stop() {
+      mediaPlayer.stop();
   }
 }
